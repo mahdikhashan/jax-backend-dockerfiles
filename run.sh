db725d36ab106b1ad5135f872fb047e920dc407b
mahdikhashan <mahdikhashan1@gmail.com>
2025-08-29 14:53:10 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ docker run --rm -it \
   --network host \
   -e JAX_PLATFORMS=cpu \
   -e XLA_FLAGS="--xla_force_host_platform_device_count=4" \
-  -v $(pwd):/workspace jax-multi-backend
+  -v "$(pwd)":/workspace \
+  jax-multi-backend \
   python3 test_multi_host.py
